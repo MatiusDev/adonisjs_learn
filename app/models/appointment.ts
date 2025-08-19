a55fb9fcbe374602de96156ea54623d0107cfcba
MatiusDev <mateomonsalve502@gmail.com>
2025-08-18 23:15:37 -0500

    NEW: Creating database models and relations

--- app/models/appointment.ts
+++ app/models/appointment.ts
@@ -1,13 +1,45 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+// Models
+import Patient from '#models/patient'
+import Doctor from '#models/doctor'
 
 export default class Appointment extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare doctorId: number
+
+  @column()
+  declare patientId: number
+
+  @column()
+  declare appointmentDte: DateTime
+
+  @column()
+  declare reason: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare status: string
+
+  @column()
+  declare paymentMethod: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @belongsTo(() => Patient)
+  declare patient: BelongsTo<typeof Patient>
+
+  @belongsTo(() => Doctor)
+  declare doctor: BelongsTo<typeof Doctor>
 }
--- app/models/patient.ts
+++ app/models/patient.ts
@@ -1,13 +1,25 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+// Models
+import Appointment from '#models/appointment'
 
 export default class Patient extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare email: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => Appointment)
+  declare appointments: HasMany<typeof Appointment>
 }
